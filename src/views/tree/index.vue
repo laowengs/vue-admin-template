b43f1f41b2559e2a9d610a42a8be1566357b36e5
laowengs <369196439@qq.com>
2021-02-04 23:16:09 +0800

    页面展示对于修改删除权限点的操作

--- src/views/tree/index.vue
+++ src/views/tree/index.vue
@@ -28,56 +28,33 @@
           :filter-node-method="filterNode"
           class="filter-tree"
           default-expand-all
-          @check-change="checkChange"
+          @check="checkChange"
         />
       </el-col>
       <el-col :span="15">
+        <h3>操作</h3>
+        <el-row :gutter="20">
+          <el-button type="primary" @click="commit">提交修改</el-button>
+          <el-button type="warning" plain @click="reset">还原权限</el-button>
+        </el-row>
         <h3>新增权限</h3>
         <el-row :gutter="20">
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
-          <el-tag closable>标签一</el-tag>
+          <el-tag
+            v-for="menu in addMenuList"
+            :key="menu.menuId"
+            closable >
+            {{menu.menuName}}
+          </el-tag>
         </el-row>
-        <br><br>
         <h3>删除权限</h3>
         <el-row :gutter="20">
-          <el-tag closable type="danger">标签四1111111111111111111111111111111111</el-tag>
-          <el-tag closable type="danger">标签四</el-tag>
-          <el-tag closable type="danger">标签四</el-tag>
-          <el-tag closable type="danger">标签四</el-tag>
-          <el-tag closable type="danger">标签四</el-tag>
-          <el-tag closable type="danger">标签四</el-tag>
-          <el-tag closable type="danger">标签四</el-tag>
-          <el-tag closable type="danger">标签四</el-tag>
-          <el-tag closable type="danger">标签四</el-tag>
-          <el-tag closable type="danger">标签四</el-tag>
-          <el-tag closable type="danger">标签四</el-tag>
-          <el-tag closable type="danger">标签四</el-tag>
-          <el-tag closable type="danger">标签四</el-tag>
-          <el-tag closable type="danger">标签四</el-tag>
-          <el-tag closable type="danger">标签四</el-tag>
-          <el-tag closable type="danger">标签四</el-tag>
+          <el-tag
+            v-for="menu in removeMenuList"
+            :key="menu.menuId"
+            type="danger"
+            closable >
+            {{menu.menuName}}
+          </el-tag>
         </el-row>
       </el-col>
     </el-row>
@@ -92,11 +69,14 @@ export default {
     return {
       filterText: '',
       checkedIdList: [],
+      firstCheckedIdList: [],
+      initCheckedIdList: [],
       menuList: [],
       // 新增的菜单信息
       addMenuList: [],
       // 删除的菜单信息
       removeMenuList: [],
+      menuInfoList: [],
       defaultProps: {
         children: 'children',
         label: 'label'
@@ -119,48 +99,128 @@ export default {
     getAllMenuInfo() {
       const allMenuInfoTree = []
       const checkId = []
+      const menuInfoList = []
       getAllMenuInfo().then(response => {
         const allMenuInfo = response.data
         allMenuInfo.forEach(menuInfo => {
           allMenuInfoTree.push({
             id: menuInfo.menuId,
             label: menuInfo.menuName,
-            children: dealMenu(menuInfo, checkId)
+            children: this.dealMenu(menuInfo, checkId, menuInfoList)
           })
-          // if (menuInfo.isAuth === 1) {
-          //   this.checkedIdList.push(menuInfo.menuId)
-          // }
+          if (menuInfo.isAuth === 1) {
+            this.firstCheckedIdList.push(menuInfo.menuId)
+          }
           // menuInfo.isAuth
           // menuInfo.childrenMenuList
         })
         this.menuList = allMenuInfoTree
         this.checkedIdList = checkId
+        this.initCheckedIdList = checkId
+        this.menuInfoList = menuInfoList
+        console.log(menuInfoList)
       })
     },
-    checkChange(data, checked, indeterminate) {
-      console.log(data, checked, indeterminate)
-    }
-  }
-}
-function dealMenu(menuInfo, checkId) {
-  const menuInfoTree = []
-  if (menuInfo) {
-    const childrenMenuList = menuInfo.childrenMenuList
-    if (childrenMenuList && childrenMenuList.length > 0) {
-      childrenMenuList.forEach(menuInfo => {
-        menuInfoTree.push({
-          id: menuInfo.menuId,
-          label: menuInfo.menuName,
-          children: dealMenu(menuInfo.childrenMenuList, checkId)
+    checkChange(node, data) {
+      // const menuId = data.checkedKeys.filter(id => {
+      //   return id === node.id
+      // })
+      // console.log(menuId)
+      // if (menuId && menuId.length > 0) {
+      //
+      // } else {
+      //
+      // }
+      const addMenu = []
+      const mvMenu = []
+      const result = this.checkMenuId()
+      result.addMenuId.forEach(id => {
+        const menuName = this.menuInfoList.filter(menu => {
+          return menu.menuId === id
         })
-        if (menuInfo.isAuth === 1) {
-          checkId.push(menuInfo.menuId)
+        addMenu.push({
+          menuId: id,
+          menuName: menuName[0].menuName
+        })
+      })
+      this.addMenuList = addMenu
+      result.deleteMenuId.forEach(id => {
+        const menuName = this.menuInfoList.filter(menu => {
+          return menu.menuId === id
+        })
+        mvMenu.push({
+          menuId: id,
+          menuName: menuName[0].menuName
+        })
+      })
+      this.removeMenuList = mvMenu
+    },
+    reset() {
+      this.$refs.tree2.setCheckedKeys([])
+      const checkedIdListReset = []
+      this.initCheckedIdList.concat(this.firstCheckedIdList).forEach(id => {
+        checkedIdListReset.push(id)
+      })
+      this.checkedIdList = checkedIdListReset
+      this.addMenuList = []
+      this.removeMenuList = []
+    },
+    checkMenuId() {
+      const currMenuIdList = this.$refs.tree2.getCheckedKeys().concat(this.$refs.tree2.getHalfCheckedKeys())
+      console.log('当前的权限点:', currMenuIdList)
+      console.log('初始化的二级权限点:', this.initCheckedIdList)
+      const initMenuIdList = [].concat(this.initCheckedIdList).concat(this.firstCheckedIdList)
+      console.log('初始化的权限点:', initMenuIdList)
+
+      const deleteMenuId = []
+      initMenuIdList.forEach(initMenuId => {
+        const result = currMenuIdList.filter(currMenuId => {
+          return currMenuId === initMenuId
+        })
+        if (result && result.length <= 0) {
+          deleteMenuId.push(initMenuId)
         }
       })
+      console.log('删除的权限点:', deleteMenuId)
+      const addMenuId = []
+      currMenuIdList.forEach(currMenuId => {
+        const result = initMenuIdList.filter(initMenuId => {
+          return currMenuId === initMenuId
+        })
+        if (result && result.length <= 0) {
+          addMenuId.push(currMenuId)
+        }
+      })
+      console.log('新增的权限点:', addMenuId)
+      return { addMenuId: addMenuId, deleteMenuId: deleteMenuId }
+    },
+    commit() {},
+    dealMenu(menuInfo, checkId, menuInfoList) {
+      const menuInfoTree = []
+      if (menuInfo) {
+        menuInfoList.push({
+          menuId: menuInfo.menuId,
+          menuName: menuInfo.menuName
+        })
+        const childrenMenuList = menuInfo.childrenMenuList
+        if (childrenMenuList && childrenMenuList.length > 0) {
+          childrenMenuList.forEach(menuInfo => {
+            menuInfoTree.push({
+              id: menuInfo.menuId,
+              label: menuInfo.menuName,
+              children: this.dealMenu(menuInfo, checkId, menuInfoList)
+            })
+            if (menuInfo.isAuth === 1) {
+              checkId.push(menuInfo.menuId)
+            }
+          })
+        }
+      }
+      return menuInfoTree
     }
   }
-  return menuInfoTree
 }
+
 </script>
 
 <style scoped>
